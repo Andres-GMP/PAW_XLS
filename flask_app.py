d29501097d685b646bc68c64cc85ea2a26cdcd4a
Andres-GMP <al20760260@ite.edu.mx>
2023-03-30 20:54:36 -0700

    refactor: just a test to view working tree in production

--- flask_app.py
+++ flask_app.py
@@ -6,13 +6,15 @@ from flask import (Flask, jsonify, render_template)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
 
+
 @app.route("/about")
 def about():
-    return "que onda"
+    return "que onda man como estás"
 
 
 @app.route("/numero_de_hoja/<file>")
