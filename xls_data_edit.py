6441925be8bb47a942c63794e69fbdd6e927c095
Andres-GMP <al20760260@ite.edu.mx>
2023-04-17 11:45:44 -0700

    fix: replace library to xlwt
    need suport to edit cell in woorkbook
    
    NOTE: Exception: Formula: unknown sheet name sheet1
    IDK why this code show this exception

--- xls_data_edit.py
+++ xls_data_edit.py
@@ -1,13 +1,19 @@
-import openpyxl
+import xlwt
 
-# name_woorkbook = '/home/Piztecho/mysite/'
-name_woorkbook = 'pagina.xls'
-woorkbook = openpyxl.load_workbook(name_woorkbook)
+def editar_celda(nombre_archivo, numero_hoja, fila, columna, valor):
+    # Abrir el archivo de Excel
+    libro = xlwt.Workbook(nombre_archivo)
 
-def edit_cell( index_sheet, rowx, columny, data):
-    sheet = woorkbook[index_sheet]
-    cell_select = sheet.cell(row=rowx, column=columny)
-    cell_select.value = data
-    woorkbook.save(name_woorkbook)
+    # Crear la hoja si no existe
+    try:
+        hoja = libro.get_sheet(numero_hoja)
+    except IndexError:
+        hoja = libro.add_sheet(numero_hoja)
 
-edit_cell('Hoja1',2, 2, 'TRUEE' )
+    # Escribir el valor en la celda especificada
+    hoja.write(fila, columna, valor)
+
+    # Guardar el archivo
+    libro.save(nombre_archivo)
+
+editar_celda('pagina.xls','sheet1', 2, 2, 'TRUEE')
